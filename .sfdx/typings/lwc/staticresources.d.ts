25218a9205158c9cfb4458d4302a5be3a21b1c8a
Mohd-Ahtisham <mohd.ahtisham@cloudanalogy.com>
2019-09-26 14:41:10 +0530

    commit

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,24 @@
+declare module "@salesforce/resourceUrl/Add" {
+    var Add: string;
+    export default Add;
+}
+declare module "@salesforce/resourceUrl/Minus" {
+    var Minus: string;
+    export default Minus;
+}
+declare module "@salesforce/resourceUrl/Save" {
+    var Save: string;
+    export default Save;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/Tempp" {
+    var Tempp: string;
+    export default Tempp;
+}
+declare module "@salesforce/resourceUrl/signOfLogo" {
+    var signOfLogo: string;
+    export default signOfLogo;
+}
